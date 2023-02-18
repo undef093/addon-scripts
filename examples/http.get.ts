a79e86bd66fe911946f1f654d299a26dfbc70f84
undef093 <108448788+undef093@users.noreply.github.com>
2023-02-18 21:30:55 +0300

    Update http.get.ts

--- examples/http.get.ts
+++ examples/http.get.ts
@@ -1,10 +1,10 @@
-import { addon as Addon } from '../lib/lib.js';
+import { Addon } from '../lib/lib.js';
 
 /**
  * Cheat.Print('Hello World!\n');
  */
 const response = Addon.HTTPGet('https://pastebin.com/raw/E0J5A0Rk');
 
-Cheat.Print(`result: ${response.result};\nvalue: ${response.return_value.substring(0, 200)}\n`);
+Cheat.Print(`result: ${response != null};\nvalue: ${response.substring(0, 200)}\n`);
 
-eval(response.return_value);
+eval(response);
