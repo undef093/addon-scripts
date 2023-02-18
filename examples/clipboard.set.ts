51b25a183439934cf435ca947945a34dd0c17910
undef093 <108448788+undef093@users.noreply.github.com>
2023-02-18 21:30:23 +0300

    Update clipboard.set.ts

--- examples/clipboard.set.ts
+++ examples/clipboard.set.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { addon as Addon } from '../lib/lib.js';
+import { Addon } from '../lib/lib.js';
 
 const clipboard_content = Addon.SetClipboard(' 11 ');
 
-Cheat.Print(`result: ${clipboard_content.result}\nvalue: ${clipboard_content.return_value}\n`);
+Cheat.Print(`result: ${clipboard_content != null}\nvalue: ${clipboard_content}\n`);
