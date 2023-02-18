9cbd1dfe3966ca2cbb08c06d9db6f2308628aee1
undef093 <108448788+undef093@users.noreply.github.com>
2023-02-18 21:31:23 +0300

    Delete render.server_hitboxes.ts

--- examples/render.server_hitboxes.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import { addon as Addon } from '../lib/lib.js';
-
-function Draw() {
-    Addon.__InternalSendJSON({ func: 'render.server_hitboxes' });
-}
-
-Cheat.RegisterCallback('Draw', 'Draw');
