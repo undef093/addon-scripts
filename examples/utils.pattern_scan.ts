1ef4adc40d40925c625274ce92856daaa180acea
undef093 <108448788+undef093@users.noreply.github.com>
2023-02-18 21:32:38 +0300

    Update utils.pattern_scan.ts

--- examples/utils.pattern_scan.ts
+++ examples/utils.pattern_scan.ts
@@ -2,4 +2,4 @@ import { addon as Addon } from "../lib/lib";
 
 const global_vars = Addon.PatternScan('client.dll', 'A1 ? ? ? 5E 8B 40 10');
 
-Cheat.Print(`result: ${global_vars.result}\naddress: 0x${(parseInt(global_vars.return_value, 16) + 1).toString(16)}\n`);
+Cheat.Print(`result: ${global_vars != null}\naddress: 0x${(parseInt(global_vars, 16) + 1).toString(16)}\n`);
