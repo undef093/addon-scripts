ce2e85075e195fe2767475203b55018777d05399
undef093 <108448788+undef093@users.noreply.github.com>
2023-02-18 21:36:18 +0300

    Update addon.d.ts

--- addon.d.ts
+++ addon.d.ts
@@ -1,9 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-type JSONResponse = { 
-    result: "1" | "0";
-    return_value: string;
-}
+type ValueOrNull = any;
 
 type ExposedFunction = 'http.get' | 'http.post' | 'clipboard.get' | 'clipboard.set' | 'filesystem.write' | 'filesystem.append' | 'filesystem.read' | 'utils.get_hwid' | 'utils.get_module_handle' | 'utils.pattern_scan' | 'render.server_hitboxes';
 
@@ -18,75 +15,28 @@ type JSONInput<F extends ExposedFunctions, N extends string, P extends string, D
 
 type DllModule = 'engine.dll' | 'client.dll' | 'vstdlib.dll' | 'vguimatsurface.dll' | 'vgui2.dll' | 'materialsystem.dll' | 'datacache.dll' | 'vphysics.dll' | 'inputsystem.dll' | 'shaderapidx9.dll';
 
-declare namespace Addon {
-    function __InternalSendJSON <F extends ExposedFunction, N extends string, P extends string> (json_obj: JSONInput<F, N, P>): JSONResponse;
+declare namespace Addon {    
+    function HTTPGet <U extends string> (url: U): ValueOrNull;
+    function HTTPPost <U extends string, D extends string> (url: U, data: D): ValueOrNull;
     
-    /**
-     * Sends an HTTP GET request.
-     */
-    function HTTPGet <U extends string> (url: U): JSONResponse;
-    
-    /**
-     * Sends an HTTP POST request.
-     */
-    function HTTPPost <U extends string, D extends string> (url: U, data: D): JSONResponse;
-    
-    /**
-     * Scans for a given byte pattern on a module.
-     */
-    function PatternScan <M extends DllModule, P extends string> (module: M, pattern: P): JSONResponse;
+    function GetClipboard(): ValueOrNull;
+    function SetClipboard <D extends string> (data: D): ValueOrNull;
+
+    function WriteFile <P extends string, D extends string> (path: P, data: D): ValueOrNull;
+    function AppendFile <P extends string, D extends string> (path: P, data: D): ValueOrNull;
+    function ReadFile <P extends string> (path: P): ValueOrNull;
 
     /**
-     * Retrieves a module handle for the specified module.
-     */
-    function GetModuleHandle <M extends DllModule> (module: M): JSONResponse;
-
-    /**
-     * Retrieves clipboard contents. 
-     */
-    function GetClipboard(): JSONResponse;
-
-    /**
-     * Sets clipboard content. 
-     */
-    function SetClipboard <D extends string> (data: D): JSONResponse;
-
-    /**
-     * Writes data to file.
-     */
-    function WriteFile <P extends string, D extends string> (path: P, data: D): JSONResponse;
-
-    /**
-     * Appends data to file.
-     */
-    function AppendFile <P extends string, D extends string> (path: P, data: D): JSONResponse;
-
-    /**
-     * Reads file contents.
-     */
-    function ReadFile <P extends string> (path: P): JSONResponse;
-
-    /**
-     * Reads registry value.
-     * 
      * @deprecated Not implemented yet.
      */
-    function ReadReg <P extends string> (path: P): JSONResponse;
+    function ReadReg <P extends string> (path: P): ValueOrNull;
     
     /**
-     * Changes registry value.
-     * 
      * @deprecated Not implemented yet.
      */
     function SetReg <P extends string> (path: P, value: string): void;
 
-    /**
-     * Returns current HWID.
-     */
-    function GetHWID(): JSONResponse;
+    function GetHWID(): ValueOrNull;
 
-    /**
-     * Returns whether addon is loaded. 
-     */
     function IsLoaded(): boolean;
 }
